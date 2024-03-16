3e1e4fe3017700b0ffa6bf49731e36ebf768908e
Peter <petergorman11@gmail.com>
2024-03-16 01:32:02 -0400

    Add client InitCache test for StringCache

--- Tests/NetBolt.Shared.Tests/StringCacheTests/ClientTests.cs
+++ Tests/NetBolt.Shared.Tests/StringCacheTests/ClientTests.cs
@@ -12,6 +12,21 @@ public sealed class ClientTests
 	private static readonly INetBoltGlue glue = new ClientGlue();
 
 	[Fact]
+	public void InitCache()
+	{
+		// Given:
+		const string entryString = "Hello, World!";
+		const uint entryId = 1;
+		var entries = ImmutableArray.Create( new KeyValuePair<string, uint>( entryString, entryId ) );
+		var stringCache = new StringCache( glue, entries );
+
+		// Then:
+		var (cachedString, cachedId) = Assert.Single( stringCache.Entries );
+		Assert.Equal( entryString, cachedString );
+		Assert.Equal( entryId, cachedId );
+	}
+
+	[Fact]
 	public void SwapEntries()
 	{
 		// Given:

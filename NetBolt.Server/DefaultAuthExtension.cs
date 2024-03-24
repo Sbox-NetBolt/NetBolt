067139d06efda7eebb029acdb9dc5fb1f98510c8
Peter <petergorman11@gmail.com>
2024-03-24 01:24:45 -0400

    Add default negotiation auth extension

---
+++ NetBolt.Server/DefaultAuthExtension.cs
@@ -0,0 +1,18 @@
+﻿using NetBolt.Server.Extensions;
+using System.Threading.Tasks;
+using vtortola.WebSockets;
+
+namespace NetBolt.Server;
+
+public sealed class DefaultAuthExtension : NetBoltServerExtension
+{
+	public override string Name => "Default Auth";
+
+	private static long GenericIdentifier;
+
+	public override ValueTask<bool> OnNegotiateSocketAsync( WebSocketHttpRequest request, WebSocketHttpResponse response )
+	{
+		request.StoreIdentifier( new ClientIdentifier( Platform.Generic, ++GenericIdentifier ), this );
+		return ValueTask.FromResult( true );
+	}
+}
